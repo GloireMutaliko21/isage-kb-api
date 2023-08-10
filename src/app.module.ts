2f9161b4eec5273ac00d2307eb15ec1456d17ee1
Gloire Mutaliko <mufunyig@gmail.com>
2023-08-10 15:10:53 +0200

    feat: create prisma service]

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { FolderElementModule } from './folder-element/folder-element.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), FolderElementModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    FolderElementModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
