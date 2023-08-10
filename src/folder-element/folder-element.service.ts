3ac584a9fa479be9dc6d1952a0c74d284daed5b7
Gloire Mutaliko <mufunyig@gmail.com>
2023-08-10 15:21:49 +0200

    feat: create get folder elements services methods

--- src/folder-element/folder-element.service.ts
+++ src/folder-element/folder-element.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class FolderElementService {}
+export class FolderElementService {
+  constructor(private prisma: PrismaService) {}
+  folderElement = this.prisma.folderElement;
+  getFolderElements() {
+    return this.folderElement.findMany();
+  }
+
+  getFolderElementById(folderElementId: string) {
+    return this.folderElement.findUnique({ where: { id: folderElementId } });
+  }
+}
