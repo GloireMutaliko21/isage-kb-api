cdc0406289e5c456b56bac03bfb56bc6f13f5cbb
Gloire Mutaliko <mufunyig@gmail.com>
2023-08-15 12:19:23 +0200

    feat: create new agent service and controller

--- src/agents/agents.controller.ts
+++ src/agents/agents.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AgentsService } from './agents.service';
+import { CreateAgentDto } from './dto';
 
 @Controller('agents')
-export class AgentsController {}
+export class AgentsController {
+  constructor(private agentService: AgentsService) {}
+
+  @Post()
+  createAgent(@Body() dto: CreateAgentDto) {
+    return this.agentService.createAgent(dto);
+  }
+}
