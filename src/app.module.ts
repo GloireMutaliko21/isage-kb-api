70631b7f43a5c71bce3d8b066aeadcb34d6fd033
Gloire Mutaliko <mufunyig@gmail.com>
2023-08-15 11:36:13 +0200

    feat: config send mail service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { FolderElementModule } from './folder-element/folder-element.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+
+import { MailModule } from './mail/mail.module';
 import { GradesModule } from './grades/grades.module';
-import { RolesModule } from './roles/roles.module';
 import { AgentsModule } from './agents/agents.module';
+import { RolesModule } from './roles/roles.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { FolderElementModule } from './folder-element/folder-element.module';
 
 @Module({
   imports: [
@@ -14,6 +17,16 @@ import { AgentsModule } from './agents/agents.module';
     RolesModule,
     PrismaModule,
     AgentsModule,
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.gmail.com',
+        auth: {
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASSWORD,
+        },
+      },
+    }),
+    MailModule,
   ],
 })
 export class AppModule {}
