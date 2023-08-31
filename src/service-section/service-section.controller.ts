87d182d952c7eccb806b155cf2fbb3e73074f575
Gloire Mutaliko <mufunyig@gmail.com>
2023-08-31 14:22:31 +0200

    feat: add sectionServices controllers

--- src/service-section/service-section.controller.ts
+++ src/service-section/service-section.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtGuard } from '../auth/guards';
+import { RolesGuard } from '../roles/guards';
+import { Role } from '../roles/enum';
+import { Roles } from '../roles/decorators';
+import { ServiceSectionService } from './service-section.service';
+import { CreateCatgoryDto } from '../property-category/dto';
 
-@Controller('service-section')
-export class ServiceSectionController {}
+@UseGuards(JwtGuard, RolesGuard)
+@Controller('service')
+@Roles(Role.Admin, Role.DuPatr)
+export class ServiceSectionController {
+  constructor(private readonly sectionService: ServiceSectionService) {}
+
+  @Post()
+  createService(@Body() dto: CreateCatgoryDto) {
+    return this.sectionService.createService(dto);
+  }
+
+  @Get()
+  getServices() {
+    return this.sectionService.getServices();
+  }
+
+  @Get(':id')
+  getServiceById(@Param('id') id: string) {
+    return this.sectionService.getServiceById(id);
+  }
+
+  @Patch(':id')
+  updateService(@Body() dto: CreateCatgoryDto, @Param('id') id: string) {
+    return this.sectionService.updateService(dto, id);
+  }
+}
